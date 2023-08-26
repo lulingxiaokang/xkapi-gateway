78fba0790f55832900ce81ed83296257f74659c2
llxk <1820472818@qq.com>
2023-08-26 17:44:42 +0800

    Initial commit

---
+++ src/main/java/com/llxk/xkapigateway/XkapiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.llxk.xkapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XkapiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XkapiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/llxk/xkapigateway/XkapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llxk.xkapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XkapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
